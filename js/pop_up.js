ecd8042e1841da12a45b82da7d765a9bb2c99769
admin <sanghyeonn4@gmail.com>
2025-06-04 21:18:02 +0900

    5주차 자바

---
+++ js/pop_up.js
@@ -0,0 +1,106 @@
+//var close_time; // 시간 정보
+//var close_time2 = 10; // 10초 설정
+//clearTimeout(close_time); // 재호출 정지
+//close_time= setTimeout("close_window()", 50000);
+// 1/1000 초 지정, 바로 시작 
+//show_time(); // 실시간 시간 보여주기
+
+//function show_time(){
+ //   let divClock = document.getElementById('Time');
+   // divClock.innerText = close_time2; // 10초 삽입 시작
+    //close_time2--; // 1초씩 감소
+ //setTimeout(show_time, 1000);  //1초마다 갱신
+  // }
+
+//function close_window() { // 함수 정의
+ //   window.close(); // 윈도우 닫기
+   // }
+
+  
+
+   function setCookie(name, value, expiredays) {
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) +
+        "; expires=" + date.toUTCString() + "; path=/";
+}
+
+
+function getCookie(name) {
+  var cookie = document.cookie;
+  console.log("쿠키를 요청합니다.");
+  if (cookie != "") {
+      var cookie_array = cookie.split("; ");
+      for (var index in cookie_array) {
+          var cookie_name = cookie_array[index].split("=");
+          if (cookie_name[0] == "popupYN") {
+              return cookie_name[1];
+          }
+      }
+  }       
+  return;
+}
+
+ 
+function closePopup() {
+  if (document.getElementById('check_popup').value) {
+    setCookie("popupYN", "N", 1);
+    console.log("쿠키를 설정합니다.");
+    self.close();
+  }
+}
+
+
+
+function pop_up() {
+      function pop_up() {
+        var cookieCheck = getCookie("popupYN");
+        if (cookieCheck != "N"){
+        window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+        }
+    }
+
+    function over(obj) {
+        obj.src="image/다운로드.png";
+        }
+        function out(obj) {
+        obj.src="image/logo2.png";
+        }
+
+
+        //const over = (obj) => {
+        //    obj.src = "image/LOGO.png";
+        //    };
+        //    const search_message = () => {
+        //    const c = '검색을 수행합니다';
+        //    alert(c);
+        //    };
+
+        function show_clock() {
+          let currentDate = new Date(); // 현재 시스템 날짜 객체 생성
+          let divClock = document.getElementById('divClock');
+          let msg = "현재시간: ";
+        
+          if (currentDate.getHours() > 12) {  // 12시보다 크면 오후, 아니면 오전
+            msg += "오후 ";
+            msg += (currentDate.getHours() - 12) + "시 ";
+          } else {
+            msg += "오전 ";
+            msg += currentDate.getHours() + "시 ";
+          }
+        
+          msg += currentDate.getMinutes() + "분 ";
+          msg += currentDate.getSeconds() + "초";
+        
+          divClock.innerText = msg;
+        
+          if (currentDate.getMinutes() > 58) {  // 정각 1분 전 빨간색 출력
+            divClock.style.color = "red";
+          } else {
+            divClock.style.color = "black"; // 아니면 기본 색상으로 복원
+          }
+        
+          setTimeout(show_clock, 1000);  // 1초마다 갱신
+        }
+        
+      }
